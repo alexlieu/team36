0a5fbd7bea36fd527bd5e394f56bd1abbd2a30e1
Alex Lieu <alieu2001@googlemail.com>
2021-05-11 14:39:57 +0100

    add a launch file for task 3 and a search_beaconing.py file which contains some of the required subscribers and publishers for task 3

--- src/search_beaconing.py
+++ src/search_beaconing.py
@@ -12,27 +12,32 @@ import cv2
 from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
 
-class object_detection:
+class search_beaconing:
     def __init__(self):
-        self.startup = False
-        self.turn_left_90 = False
-        self.turn_left_180 = False
-        self.turn_right_180 = False
+        self.detect_colour = False
         self.found_colour = False
-        self.forward = True
 
         self.forward_speed = 0.2
         self.turn_speed = 0.3
 
-        rospy.init_node('avoid_node', anonymous=True)
+        rospy.init_node('search_beaconing_node', anonymous=True)
         self.rate = rospy.Rate(10)
-        self.camera_subscriber = rospy.Subscriber("/camera/rgb/image_raw", Image, self.camera_callback)
+        self.camera_sub = rospy.Subscriber("/camera/rgb/image_raw", Image, self.camera_callback)
+        self.scan_sub = rospy.Subscriber('/scan', LaserScan, self.scan_callback)
         self.cvbridge_interface = CvBridge()
         self.robot_controller = MoveTB3()
         self.robot_odom = TB3Odometry()
 
         self.ctrl_c = False
         rospy.on_shutdown(self.shutdownhook)
+
+        self.front_min_distance = 0.0
+        self.left_min_distance = 0.0
+        self.right_min_distance = 0.0
+
+        self.front_min_angle = 0.0
+        self.left_min_angle = 0.0
+        self.right_min_angle = 0.0
 
         self.m00 = 0
         self.m00_min = 10000
@@ -59,7 +64,7 @@ class object_detection:
         crop_img = cv_img[crop_y:crop_y+crop_height, crop_x:crop_x+crop_width]
         hsv_img = cv2.cvtColor(crop_img, cv2.COLOR_BGR2HSV)
 
-        if self.startup:
+        if self.detect_colour:
             white_space = [None]*6
             for i in range(6):
                 mask = cv2.inRange(hsv_img, self.lower[i], self.upper[i])
@@ -67,8 +72,7 @@ class object_detection:
             self.max_white_space_index = white_space.index(max(white_space))
             self.mask = cv2.inRange(hsv_img, self.lower[self.max_white_space_index], self.upper[self.max_white_space_index])
             print("SEARCH INITIATED: The target colour is " + self.colours[self.max_white_space_index])
-            self.turn_left_90 = True
-            self.startup = False
+            self.detect_colour = False
         else:
             self.mask = cv2.inRange(hsv_img, self.lower[self.max_white_space_index], self.upper[self.max_white_space_index])
 
@@ -83,6 +87,30 @@ class object_detection:
         cv2.imshow('cropped image', crop_img)
         cv2.waitKey(1)
 
+    def scan_callback(self, scan_data):
+        angle_tolerance = 5
+
+        front_left_arc = scan_data.ranges[0:21]
+        front_right_arc = scan_data.ranges[-20:]
+        front_front_arc = np.array(front_left_arc[::-1] + front_right_arc[::-1])
+        front_arc_angles = np.arange(-20, 21)
+        self.front_min_distance = front_front_arc.min()
+        self.front_min_angle = front_arc_angles[np.argmin(front_front_arc)]
+
+        left_left_arc = scan_data.ranges[40:61]
+        left_right_arc = scan_data.ranges[21:40]
+        left_front_arc = np.array(left_left_arc[::-1] + left_right_arc[::-1])
+        left_arc_angles = np.arange(-61, -21)
+        self.left_min_distance = left_front_arc.min()
+        self.left_min_angle = left_arc_angles[np.argmin(left_front_arc)]
+
+        right_left_arc = scan_data.ranges[-40:-20]
+        right_right_arc = scan_data.ranges[-60:-40]
+        right_front_arc = np.array(right_left_arc[::-1] + right_right_arc[::-1])
+        right_arc_angles = np.arange(20, 60)
+        self.right_min_distance = right_front_arc.min()
+        self.right_min_angle = right_arc_angles[np.argmin(right_front_arc)]
+
     def shutdownhook(self):
         self.shutdown_function()
         self.ctrl_c = True
@@ -92,49 +120,11 @@ class object_detection:
 
     def main_loop(self):
         while not self.ctrl_c:
-            if self.forward == True:
-                self.robot_controller.set_move_cmd(linear=self.forward_speed)
-                if sqrt(pow(self.robot_odom.posx0 - self.robot_odom.posx, 2) + pow(self.robot_odom.posy0 - self.robot_odom.posy, 2)) >= 1:
-                    self.robot_controller.stop()
-                    self.robot_odom.posx0 = self.robot_odom.posx
-                    self.robot_odom.posy0 = self.robot_odom.posy
-                    self.forward = False
-                    self.turn_right_180 = True
-            elif self.turn_right_180 == True:
-                self.robot_controller.set_move_cmd(angular=-self.turn_speed)
-                if abs(self.robot_odom.yaw0 - self.robot_odom.yaw) >= 180:
-                    self.robot_controller.stop()
-                    self.robot_odom.yaw0 = self.robot_odom.yaw
-                    self.turn_right_180 = False
-                    self.startup = True
-            elif self.turn_left_90 == True:
-                self.robot_controller.set_move_cmd(angular=self.turn_speed)
-                if abs(self.robot_odom.yaw0 - self.robot_odom.yaw) >= 90:
-                    self.robot_controller.stop()
-                    self.robot_odom.yaw0 = self.robot_odom.yaw
-                    self.turn_left_90 = False
-                    self.turn_left_180 = True
-            elif self.turn_left_180 == True:
-                self.robot_controller.set_move_cmd(angular=self.turn_speed)
-                if self.m00 > self.m00_min:
-                    if self.cy >= 560-100 and self.cy <= 560+100:
-                        self.robot_controller.stop()
-                        self.turn_left_180 = False
-                        self.found_colour = True
-                if abs(self.robot_odom.yaw0 - self.robot_odom.yaw) >= 180:
-                    self.robot_controller.stop()
-                    self.robot_odom.yaw0 = self.robot_odom.yaw
-                    self.turn_left_180 = False
-                    print("SEARCH FAILED: A pillar of target colour " + self.colours[self.max_white_space_index] + " is not present.")
-                    self.ctrl_c = True
-            elif self.found_colour == True:
-                print("SEARCH COMPLETE: The robot is now facing the target pillar.")
-                self.ctrl_c = True
-            self.robot_controller.publish()
+            print("Working")
 
 if __name__ == '__main__':
-    object_detection_instance = object_detection()
+    search_beaconing_instance = search_beaconing()
     try:
-        object_detection_instance.main_loop()
+        search_beaconing_instance.main_loop()
     except rospy.ROSInterruptException:
         pass
